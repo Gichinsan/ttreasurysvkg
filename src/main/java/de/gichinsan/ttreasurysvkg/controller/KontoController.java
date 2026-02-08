80e68207b8a0910318d68eb0030de814db9b7ef6
Gichinsan <durstdennis@gmail.com>
2026-02-08 16:51:38 +0100

    Code fixes

--- src/main/java/de/gichinsan/ttreasurysvkg/controller/KontoController.java
+++ src/main/java/de/gichinsan/ttreasurysvkg/controller/KontoController.java
@@ -8,9 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -32,11 +30,8 @@ public class KontoController extends BaseController {
     @PostMapping("/konto")
     public String updateKontoBeschreibung(@AuthenticationPrincipal UserDetails userDetails,
                                           @RequestParam String kontoBeschreibung,
-                                          @Valid @ModelAttribute("user") ClubManagerUser user,
-                                          BindingResult bindingResult,
                                           RedirectAttributes redirectAttributes) {
-        user = getCurrentClubManagerUser(userDetails);
-
+        @Valid ClubManagerUser user = getCurrentClubManagerUser(userDetails);
         user.setKontoBeschreibung(kontoBeschreibung);
         userRepository.save(user);
         redirectAttributes.addFlashAttribute("success", "Beschreibung erfolgreich aktualisiert");
